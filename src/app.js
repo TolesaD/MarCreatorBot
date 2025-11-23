0a15978bfe1959018daafbe1b4d39bc9d799459a
Tolesa <tolesadebushe9@gmail.com>
2025-11-24 01:40:30 +0300

    COMPLEX

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ if (isCpanel) {
 }
 
 const { Telegraf, Markup } = require('telegraf');
-const config = require('./config/environment');
-const { connectDB } = require('./database/db');
+const config = require('../config/environment');
+const { connectDB } = require('../database/db');
 const MiniBotManager = require('./services/MiniBotManager');
 
 const { startHandler, helpHandler, featuresHandler } = require('./handlers/startHandler');
